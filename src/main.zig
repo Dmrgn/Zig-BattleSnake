7326613d06f9d4ca10ae2dab2fbf79199367e0af
Daniel Morgan <danielrmorgan11@gmail.com>
2025-12-06 22:16:43 -0500

    Host on 0.0.0.0 instead of localhost

--- src/main.zig
+++ src/main.zig
@@ -16,7 +16,7 @@ pub fn main() !void {
     // More advance cases will use a custom "Handler" instead of "void".
     // The last parameter is our handler instance, since we have a "void"
     // handler, we passed a void ({}) value.
-    var server = try httpz.Server(void).init(allocator, .{ .port = 5882 }, {});
+    var server = try httpz.Server(void).init(allocator, .{ .port = 5882, .address = "0.0.0.0" }, {});
     defer {
         // clean shutdown, finishes serving any live request
         server.stop();
